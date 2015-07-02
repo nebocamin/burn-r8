70c7d269f04d27c2dff83c63009154a914058c70
Johannes Gest <Johannes.Gest@haufe-lexware.com>
2015-07-02 14:58:47 +0200

    added some more detailed burndowns, improved styling & updated numbers

--- js/index.js
+++ js/index.js
@@ -2,9 +2,7 @@ $(function () {
   $('#container').highcharts({
     title: {
       text: 'Bug Burndown',
-      x: -20 //center
     },
-    colors: ['blue', 'green'],
     plotOptions: {
       line: {
         lineWidth: 3
@@ -13,16 +11,13 @@ $(function () {
         hideDelay: 200
       }
     },
-    subtitle: {
-      text: 'fupp',
-      x: -20
-    },
     xAxis: {
       categories: ['Day 31', 'Day 30', 'Day 29','Day 28','Day 27','Day 26','Day 25','Day 24','Day 23','Day 22','Day 21','Day 20','Day 19','Day 18','Day 17','Day 16','Day 15','Day 14','Day 13','Day 12','Day 11','Day 10','Day 9','Day 8', 'Day 7', 'Day 6', 'Day 5', 'Day 4', 'Day 3', 'Day 2', 'Day 1','Day 0',]
     },
     yAxis: {
+      min: 0,
       title: {
-        text: 'Hours'
+        text: null
       },
       plotLines: [{
         value: 0,
@@ -34,24 +29,41 @@ $(function () {
       crosshairs: true,
       shared: true
     },
-    legend: {
-      layout: 'vertical',
-      align: 'right',
-      verticalAlign: 'middle',
-      borderWidth: 0
-    },
-    series: [{
-      name: 'Ideal Burn',
-      color: 'rgba(0,255,0,0.25)',
-      lineWidth: 2,
-      data: [63, 61, 59, 57,55,53,51,49,47,45,43,41,39,37,35,33,31,29,27,25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
-    }, {
-      name: 'Actual Burn',
-      color: 'rgba(0,120,200,0.75)',
-      marker: {
-        radius: 6
-      },
-      data: [63,67,65,65]
-    }]
+    series: [
+      {
+        name: 'Ideal',
+        color: 'rgba(0,255,0,0.25)',
+        lineWidth: 1,
+        data: [63, 61, 59, 57,55,53,51,49,47,45,43,41,39,37,35,33,31,29,27,25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
+      }, {
+        name: 'Actual',
+        color: 'rgba(0,120,200,0.75)',
+        marker: {
+          radius: 10
+        },
+        data: [63,67,65,61]
+      }, {
+        name: 'Pending',
+        color: 'rgba(0,200,200,0.75)',
+        marker: {
+          radius: 2
+        },
+        data: [63,53,53,53]
+      }, {
+        name: 'High & Critical',
+        color: 'rgba(225,0,0,0.75)',
+        marker: {
+          radius: 2
+        },
+        data: [20,19,17,17]
+      }, {
+        name: 'Normal',
+        color: 'rgba(225,200,0,0.75)',
+        marker: {
+          radius: 2
+        },
+        data: [25,25,23,23]
+      }
+    ]
   });
 });
